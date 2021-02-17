289ca62b8519e92f8b0e7e12ad3c910f1a3ff04b
Michael Dimitras <michaeldimitras@Michaels-MBP.attlocal.net>
2021-02-17 10:38:19 -0800

    Added start script

--- index.js
+++ index.js
@@ -13,22 +13,26 @@ const runner = (data) => {
 
     //assumes first row is headers
     for (let i = 1; i < data.length; i++) {
-        if(!Array.isArray(data[i])) {
-            console.error(`Line ${i} of data is not an array. Intead it is ${typeof data[i]}`);
-        } else if (data.length < 2) {
-            console.error(`Missing data for row ${i} of file`);
-        } else {
-            //cb is message function
-            const targetPrice = data[i][0];
-            const url = data[i][1];
-
-            if (urlLog.hasItem(url)) {
-                console.log(`Message already sent for ${url}`);
+        try {
+            if(!Array.isArray(data[i])) {
+                console.error(`Line ${i} of data is not an array. Intead it is ${typeof data[i]}`);
+            } else if (data.length < 2) {
+                console.error(`Missing data for row ${i} of file`);
             } else {
-                priceChecker(data[i][1], data[i][0], sendMessage);
-                urlLog.addItem(url);
+                //cb is message function
+                const targetPrice = data[i][0];
+                const url = data[i][1];
+    
+                if (urlLog.hasItem(url)) {
+                    console.log(`Message already sent for ${url}`);
+                } else {
+                    priceChecker(data[i][1], data[i][0], sendMessage);
+                    urlLog.addItem(url);
+                }
+    
             }
-
+        } catch(err) {
+            console.error(`Error for sending message for row ${i}`);
         }
     }
 }
